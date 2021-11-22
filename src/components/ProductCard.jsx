134ac4487950fd90aa6699849a4b130cdd075fa6
ThanhAn <fxannguyen201@gmail.com>
2021-11-22 16:01:56 +0700

    Add Lazy Load Image And Custom Slide Banner

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,45 +1,72 @@
-import React,{useState} from 'react'
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import { Link } from "react-router-dom";
+import numberWithCommans from "../utils/numberWithCommans";
+import Button from "./Button";
 
-import { Link } from 'react-router-dom'
-
-import Button from './Button'
-
-import numberWithCommans from '../utils/numberWithCommans'
-
-const ProductCard = props => {
-
-  const [sourceURL,setSourceURL] = useState(props.img01);
+const ProductCard = (props) => {
+  const [sourceURL, setSourceURL] = useState(props.img01);
 
   return (
     <div className="product-card">
       <div className="container-product-card">
         <ul className="thumb">
-          <li className='child-shoes'><img src={props.img01} alt="img01" onClick={()=> setSourceURL(props.img01)}/></li>
-          <li className='child-shoes'><img src={props.img02} alt="img02" onClick={()=> setSourceURL(props.img02)}/></li>
+          <li className="child-shoes">
+            <LazyLoadImage
+              src={props.img01}
+              alt="img01"
+              onClick={() => setSourceURL(props.img01)}
+              effect="blur"
+              height="80%"
+              width="80%"
+              delayTime={500}
+              placeholderSrc={process.env.PUBLIC_URL + "/logo192.png"}
+            />
+          </li>
+          <li className="child-shoes">
+            <LazyLoadImage
+              src={props.img02}
+              alt="img02"
+              onClick={() => setSourceURL(props.img02)}
+              effect="blur"
+              height="80%"
+              width="80%"
+              delayTime={500}
+              placeholderSrc={process.env.PUBLIC_URL + "/logo192.png"}
+            />
+          </li>
         </ul>
         <div className="imgBox">
           <h2>{props.name}</h2>
           <Link to={`/catalog/${props.slug}`}>
-            <img src={sourceURL} alt="img" className="shoesss"/>
+            <LazyLoadImage
+              src={sourceURL}
+              alt="img"
+              effect="blur"
+              className="shoess"
+              // height="80%"
+              // width="80%"
+              delayTime={500}
+              placeholderSrc={process.env.PUBLIC_URL + "/logo192.png"}
+            />
           </Link>
           <ul className="size">
             <span>Giá</span>
             <li>{numberWithCommans(props.price)}&#x00111;</li>
-            <li><del>{numberWithCommans(399000)} &#x00111;</del></li>
+            <li>
+              <del>{numberWithCommans(399000)} &#x00111;</del>
+            </li>
           </ul>
-          <Button
-            size="sm"
-            icon="bx bx-cart"
-            animate={true}
-          >
+          <Button size="sm" icon="bx bx-cart" animate={true}>
             chọn mua
           </Button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 ProductCard.propTypes = {
   img01: PropTypes.string.isRequired,
@@ -47,6 +74,6 @@ ProductCard.propTypes = {
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
   slug: PropTypes.string.isRequired,
-}
+};
 
-export default ProductCard
+export default ProductCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,51 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-import Helmet from '../components/Helmet'
-import HeroSlider from '../components/HeroSlider'
-
-import Section, { SectionBody, SectionTitle } from '../components/Section'
-import PolicyCard from '../components/PolicyCard'
-import Grid from '../components/Grid'
-import ProductCard from '../components/ProductCard'
-
-import heroSliderData from '../assets/fake-data/hero-slider'
-import policy from '../assets/fake-data/policy'
-import productData from '../assets/fake-data/products'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import heroSliderData from "../assets/fake-data/hero-slider";
+import policy from "../assets/fake-data/policy";
+import productData from "../assets/fake-data/products";
+import Grid from "../components/Grid";
+import Helmet from "../components/Helmet";
+import HeroSlider from "../components/HeroSlider";
+import PolicyCard from "../components/PolicyCard";
+import ProductCard from "../components/ProductCard";
+import Section, { SectionBody, SectionTitle } from "../components/Section";
 // import banner from '../assets/images/banner.png'
-import SlideBanner from '../components/SlideBanner'
-
-
+import SlideBanner from "../components/SlideBanner";
 
 const Home = () => {
   return (
-    <Helmet title='Trang chủ'>
+    <Helmet title="Trang chủ">
       {/* {hero slider} */}
       <HeroSlider
         data={heroSliderData}
         control={true}
         auto={false}
-        timeOut={1000} />
+        timeOut={1000}
+      />
       {/* {end hero slider} */}
       {/* policy section */}
       <Section>
         <SectionBody>
-          <Grid
-            col={4}
-            mdCol={2}
-            smCol={1}
-            gap={20}
-          >
-            {
-              policy.map((item, index) => <Link key={index} to="/policy">
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {policy.map((item, index) => (
+              <Link key={index} to="/policy">
                 <PolicyCard
                   name={item.name}
                   description={item.description}
                   icon={item.icon}
-                />
-              </Link>)
-            }
+                ></PolicyCard>
+              </Link>
+            ))}
           </Grid>
         </SectionBody>
       </Section>
@@ -53,29 +43,19 @@ const Home = () => {
 
       {/* best selling section */}
       <Section>
-        <SectionTitle>
-          top sản phẩm bản chạy trong tuần
-        </SectionTitle>
+        <SectionTitle>top sản phẩm bản chạy trong tuần</SectionTitle>
         <SectionBody>
-          <Grid
-            col={4}
-            mdCol={2}
-            smCol={1}
-            gap={20}
-          >
-            {
-              productData.getProducts(4).map((item, index) => (
-                <ProductCard
-                  key={index}
-                  img01={item.image01}
-                  img02={item.image02}
-                  name={item.title}
-                  price={Number(item.price)}
-                  slug={item.slug}
-                
-                />
-              ))
-            }
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {productData.getProducts(4).map((item, index) => (
+              <ProductCard
+                key={index}
+                img01={item.image01}
+                img02={item.image02}
+                name={item.title}
+                price={Number(item.price)}
+                slug={item.slug}
+              />
+            ))}
           </Grid>
         </SectionBody>
       </Section>
@@ -83,28 +63,19 @@ const Home = () => {
 
       {/* new arrival section */}
       <Section>
-        <SectionTitle>
-          sản phẩm mới
-        </SectionTitle>
+        <SectionTitle>sản phẩm mới</SectionTitle>
         <SectionBody>
-          <Grid
-            col={4}
-            mdCol={2}
-            smCol={1}
-            gap={20}
-          >
-            {
-              productData.getProducts(8).map((item, index) => (
-                <ProductCard
-                  key={index}
-                  img01={item.image01}
-                  img02={item.image02}
-                  name={item.title}
-                  price={Number(item.price)}
-                  slug={item.slug}
-                />
-              ))
-            }
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {productData.getProducts(8).map((item, index) => (
+              <ProductCard
+                key={index}
+                img01={item.image01}
+                img02={item.image02}
+                name={item.title}
+                price={Number(item.price)}
+                slug={item.slug}
+              />
+            ))}
           </Grid>
         </SectionBody>
       </Section>
@@ -114,8 +85,8 @@ const Home = () => {
       <Section>
         <SectionBody>
           {/* <Link to="/catalog"> */}
-            {/* <img src={banner} alt="" /> */}
-            <SlideBanner/>
+          {/* <img src={banner} alt="" /> */}
+          <SlideBanner />
           {/* </Link> */}
         </SectionBody>
       </Section>
@@ -123,34 +94,25 @@ const Home = () => {
 
       {/* popular product */}
       <Section>
-        <SectionTitle>
-          phổ biến
-        </SectionTitle>
+        <SectionTitle>phổ biến</SectionTitle>
         <SectionBody>
-          <Grid
-            col={4}
-            mdCol={2}
-            smCol={1}
-            gap={20}
-          >
-            {
-              productData.getProducts(12).map((item, index) => (
-                <ProductCard
-                  key={index}
-                  img01={item.image01}
-                  img02={item.image02}
-                  name={item.title}
-                  price={Number(item.price)}
-                  slug={item.slug}
-                />
-              ))
-            }
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {productData.getProducts(12).map((item, index) => (
+              <ProductCard
+                key={index}
+                img01={item.image01}
+                img02={item.image02}
+                name={item.title}
+                price={Number(item.price)}
+                slug={item.slug}
+              />
+            ))}
           </Grid>
         </SectionBody>
       </Section>
       {/* end popular product */}
     </Helmet>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
