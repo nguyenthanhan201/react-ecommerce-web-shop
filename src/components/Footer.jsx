1c595f4d0011baa25cb423a4a9668a01fb8b9e4c
chiennguyen040300 <chiennguyen040300@gmail.com>
2021-12-03 21:21:26 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import Grid from './Grid';
 
 import logo from '../assets/images/Logo-2.png'
-
+// Thêm CSS và tạo bố cục Footer 
 const footerAboutLinks = [
   {
     display: "Giới Thiệu",
