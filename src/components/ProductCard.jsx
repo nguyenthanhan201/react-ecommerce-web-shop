717ba715d08b8f03bbf2e6e24b20cb1633420727
Mai Xuan Huynh <19211tt2049@mail.tdc.edu.vn>
2021-12-03 17:35:45 +0700

    update

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 import Button from './Button'
 
 import numberWithCommans from '../utils/numberWithCommans'
-
+//add
 const ProductCard = props => {
   return (
     <div className="product-card">
