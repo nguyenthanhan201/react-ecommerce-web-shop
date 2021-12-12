1d5f04f44c2c079e7ea2b9e142638cb66fe1a411
ThanhAn <fxannguyen201@gmail.com>
2021-12-12 14:27:01 +0700

    update

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -3,6 +3,7 @@ import React, { useCallback, useState, useEffect, useRef } from "react";
 import Helmet from "../components/Helmet";
 import CheckBox from "../components/CheckBox";
 import Button from "../components/Button";
+//
 
 import productData from "../assets/fake-data/products";
 import category from "../assets/fake-data/category";
